5d5cd8e5ee7a4e14f218bb6b36790da5f5a30122
Georgios Karametas <geometas@hotmail.com>
2017-11-20 18:37:00 +0100

    Minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
 	return (
 		<footer className="footer">
 			<p>API by Brian Fritz.</p>
 			<p>All content licensed under <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">CC BY-NC 4.0.</a></p>
-			<p>This site is not endorsed by or affiliated with <a href="http://www.imdb.com/" target="_blank">IMDb.com.</a></p>
+			<p>www.omdbapi.com is not endorsed by or affiliated with <a href="http://www.imdb.com/" target="_blank">IMDb.com.</a></p>
 		</footer>
 	)
 }
