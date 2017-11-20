57d2061d854c2b07801e5a700cdcd91893ee06aa
Georgios Karametas <geometas@hotmail.com>
2017-11-20 18:38:53 +0100

    Fixed warning about target blank in footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<p>API by Brian Fritz.</p>
-			<p>All content licensed under <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">CC BY-NC 4.0.</a></p>
-			<p>www.omdbapi.com is not endorsed by or affiliated with <a href="http://www.imdb.com/" target="_blank">IMDb.com.</a></p>
+			<p>All content licensed under <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC 4.0.</a></p>
+			<p>www.omdbapi.com is not endorsed by or affiliated with <a href="http://www.imdb.com/" target="_blank" rel="noopener noreferrer">IMDb.com.</a></p>
 		</footer>
 	)
 }
