8076ca3904ae27555ddbc50c3d23eaf1a8404a2d
Georgios Karametas <geometas@hotmail.com>
2017-11-20 11:35:43 +0100

    Added some basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from '../assets/images/omdb.png'
+import './Header.css'
 
 const Header = () => {
 	return (
 		<header className="header">
-			<img src={logo} alt="The Open Movie Database" />
+			<img className="logo" src={logo} alt="The Open Movie Database" />
 		</header>
 	)
 }
