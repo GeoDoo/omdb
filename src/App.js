2bcd7569a11c210bf7f77b9c25d4e04d25c14f2a
Georgios Karametas <geometas@hotmail.com>
2017-11-20 12:17:20 +0100

    Added .env for hiding the API KEY

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import api from './helpers/api'
 import './App.css';
 
 class App extends Component {
+	componentWillMount() {
+		api.fetchMoviesByStringSearch()
+			.then(json => {
+				console.log(json)
+			})
+	}
+
   render() {
     return (
       <div className="app">
