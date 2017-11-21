b80161413f38fd1feaf8bc18ee1cec5917118031
Georgios Karametas <geometas@hotmail.com>
2017-11-21 21:59:56 +0100

    Added enzyme and edited app test to pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 test('renders without crashing', () => {
