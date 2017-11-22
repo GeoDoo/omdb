fe7c5394902762702372196243794ee4ad8905bc
Georgios Karametas <geometas@hotmail.com>
2017-11-22 15:52:02 +0100

    Added some tests for Main

--- src/components/Main.test.js
+++ src/components/Main.test.js
@@ -1,7 +1,60 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
+import { MemoryRouter } from 'react-router'
 import Main from './Main'
+import NotFound from './NotFound'
+import Home from '../containers/Home'
+import ResultInfoPage from '../containers/ResultInfoPage'
+
+describe('Navigates properly', () => { 
+
+	describe('to Home page', () => {
+		test('invalid path should redirect to 404', () => {
+		  const wrapper = mount(
+		    <MemoryRouter initialEntries={[ '/random' ]}>
+		      <Main />
+		    </MemoryRouter>
+		  )
+		  expect(wrapper.find(Home)).toHaveLength(0)
+		  expect(wrapper.find(NotFound)).toHaveLength(1)
+		})
+
+		test('valid path should not redirect to 404', () => {
+		  const wrapper = mount(
+		    <MemoryRouter initialEntries={[ '/' ]}>
+		      <Main />
+		    </MemoryRouter>
+		  )
+		  expect(wrapper.find(Home)).toHaveLength(1)
+		  expect(wrapper.find(NotFound)).toHaveLength(0)
+		})
+	})
+
+	describe('to Result page', () => {
+		test('invalid path should redirect to 404', () => {
+		  const wrapper = mount(
+		    <MemoryRouter initialEntries={[ '/random' ]}>
+		      <Main />
+		    </MemoryRouter>
+		  )
+		  expect(wrapper.find(ResultInfoPage)).toHaveLength(0)
+		  expect(wrapper.find(NotFound)).toHaveLength(1)
+		})
+
+		// test('valid path should not redirect to 404', () => {
+		//   const wrapper = mount(
+		//     <MemoryRouter initialEntries={[ '/imdb-id/tt0062622' ]}>
+		//       <Main />
+		//     </MemoryRouter>
+		//   )
+		//   expect(wrapper.find(ResultInfoPage)).toHaveLength(1)
+		//   expect(wrapper.find(NotFound)).toHaveLength(0)
+		// })
+	})
+
+})
 
 test('renders without crashing', () => {
 	shallow(<Main />)
 })
+
